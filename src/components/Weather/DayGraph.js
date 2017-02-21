3eb1f49344afbe828c88f90bbfa4bb4b111ef979
Justin Heideman <justin.heideman@nytimes.com>
2017-02-21 15:17:34 -0500

    Initial work on weather stuff

---
+++ src/components/Weather/DayGraph.js
@@ -0,0 +1,30 @@
+import React, { Component, PropTypes } from 'react';
+import styles from './styles.scss';
+
+
+class DayGraph extends Component {
+  // constructor (props) {
+  //   super(props);
+  // }
+
+  getPrettyDate() {
+    const date = new Date(this.props.forecast.Date);
+    const options = { weekday: 'long', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString('en-US', options);
+  }
+
+
+  render() {
+    return (<p><strong>{this.getPrettyDate()}:</strong> Low of {this.props.forecast.Temperature.Minimum.Value}º and a high of {this.props.forecast.Temperature.Maximum.Value}º.</p>);
+  }
+}
+
+
+DayGraph.propTypes = {
+  min: PropTypes.number.isRequired,
+  max: PropTypes.number.isRequired,
+  forecast: PropTypes.object.isRequired
+};
+
+
+export default DayGraph;
--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -1,11 +1,37 @@
 
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import styles from './styles.scss';
 
-function Weather() {
-  return (
-    <h2>I am the weather</h2>
-  );
+import Forecast, { min, max } from '../../forecast';
+import DayGraph from './DayGraph';
+
+
+//console.log(Forecast, min, max);
+
+// https://dataservice.accuweather.com/forecasts/v1/daily/5day/349727?apikey=
+
+class Weather extends Component {
+
+
+    getDayGraphs() {
+        let i = 0;
+        return Forecast.map((f) => {
+            i++;
+            return <DayGraph max={max} min={min} forecast={f} key={i}/>
+        });
+    }
+
+    render() {
+        return (<div>
+            <h2>Weather for the next 5 days:</h2>
+            <ul>
+                <li>Low: {min}º</li>
+                <li>High: {max}º</li>
+            </ul>
+            {this.getDayGraphs()}
+        </div>
+      );
+    }
 }
 
 export default Weather;
